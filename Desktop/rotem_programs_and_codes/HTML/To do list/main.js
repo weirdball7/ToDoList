bd48199b705319b6be164ed23357fa2b701729c8
WeirdBall77 <120051405+WeirdBall77@users.noreply.github.com>
2023-10-01 00:27:06 +0300

    Started Move to django

--- Desktop/rotem_programs_and_codes/HTML/To do list/main.js
+++ Desktop/rotem_programs_and_codes/HTML/To do list/main.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const taskName = document.getElementById('task-input').value;
+    
     const task = document.getElementById('task-container');
     // const parent = task.parentNode;
-    const taskClone = task.cloneNode(true);
+    
     const updateButton = document.getElementById('update');
-    document.removeChild(task);
+    // document.body.removeChild(task);
 
     updateButton.addEventListener('click', createTask);
 
     function createTask() {
+        const taskName = document.getElementById('task-input').value;
+        const taskClone = task.cloneNode(true);
         let taskTitle = taskClone.firstChild;
         taskTitle.innerHTML = taskName;
         document.body.appendChild(taskClone);
-}
+    }
 });
