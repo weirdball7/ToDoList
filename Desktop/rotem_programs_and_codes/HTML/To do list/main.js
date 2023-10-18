d45a632f454c796fea3c4b689d9d880223564ab7
WeirdBall77 <120051405+WeirdBall77@users.noreply.github.com>
2023-10-18 22:31:33 +0300

    made the js work u can now add a new task but the buttons on them dont work

--- Desktop/rotem_programs_and_codes/HTML/To do list/main.js
+++ Desktop/rotem_programs_and_codes/HTML/To do list/main.js
@@ -1,18 +1,50 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
-    const task = document.getElementById('task-container');
-    // const parent = task.parentNode;
-    
-    const updateButton = document.getElementById('update');
-    // document.body.removeChild(task);
+    // Get references to the HTML elements
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
+    const updateButton = document.getElementById("update");
 
-    updateButton.addEventListener('click', createTask);
+    // Add an event listener to the "Update" button
+    updateButton.addEventListener("click", addTask);
 
-    function createTask() {
-        const taskName = document.getElementById('task-input').value;
-        const taskClone = task.cloneNode(true);
-        let taskTitle = taskClone.firstChild;
-        taskTitle.innerHTML = taskName;
-        document.body.appendChild(taskClone);
+    function addTask() {
+        // Get the task text from the input field
+        const newTaskText = taskInput.value;
+
+        if (newTaskText.trim() !== "") {
+            // Create a new task container div
+            const taskContainer = document.createElement("div");
+            taskContainer.classList.add("task-container");
+
+            // Create a paragraph for the task text
+            const taskDetail = document.createElement("p");
+            taskDetail.textContent = newTaskText;
+            taskDetail.classList.add("task-detail");
+
+            // Create buttons for task options
+            const taskOption1 = document.createElement("button");
+            taskOption1.textContent = "Not Done";
+            taskOption1.classList.add("task-option1");
+
+            const taskOption2 = document.createElement("button");
+            taskOption2.textContent = "In Progress";
+            taskOption2.classList.add("task-option2");
+
+            const taskOption3 = document.createElement("button");
+            taskOption3.textContent = "Done";
+            taskOption3.classList.add("task-option3");
+
+            // Append elements to the task container
+            taskContainer.appendChild(taskDetail);
+            taskContainer.appendChild(taskOption1);
+            taskContainer.appendChild(taskOption2);
+            taskContainer.appendChild(taskOption3);
+
+            // Append the new task container to the task list
+            taskList.appendChild(taskContainer);
+
+            // Clear the input field
+            taskInput.value = "";
+        }
     }
 });
